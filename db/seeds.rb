581279fb5264f6c6c3318e959237ef6a32e28e38
Bojan Oro <boro@ryerson.ca>
2019-03-31 21:31:34 -0400

    Remove create users from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(email: "test@test.com", password: "lol123")
-User.create(email: "a@b.com", password: "lol123")
-User.create(email: "dick@butt.com", password: "lol123")
+# User.create(email: "test@test.com", password: "lol123")
+# User.create(email: "a@b.com", password: "lol123")
+# User.create(email: "dick@butt.com", password: "lol123")
 
 # Game.create()
 # Game.create()
